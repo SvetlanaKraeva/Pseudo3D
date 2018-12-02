57e8641832381c7a8cf3f10c079e09fa7ade40a2
SvetlanaKraeva <kraeva.so96@gmail.com>
2018-12-02 18:00:12 +0300

    Added symmetry

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ end_length = np.amax(data_quasi, axis=0)[-2]
 index_max = np.argmax(data_quasi, axis=0)[-2]
 height_max = max(data_quasi[index_max + 1][-2], abs(data_quasi[index_max + 1][-1]), np.amax(data_events, axis=0)[3],
                  -np.amin(data_events, axis=0)[3])
-length_max = max(end_length, np.amax(data_events, axis=0)[3])
+length_max = max(end_length, np.amax(abs(data_events), axis=0)[3])
 opening_max = data_quasi[index_max+1][1]
 opening_min = np.amin(data_quasi, axis=0)[1]
 
@@ -48,7 +48,12 @@ for plot_index in quasi_stages:
                           [x_min, y_max],
                           [x_max, y_max],
                           [x_max, y_min]])
-        patches.append(Polygon(coord, True))
+        coord_sym = np.array([[-x_min, y_min],
+                              [-x_min, y_max],
+                              [-x_max, y_max],
+                              [-x_max, y_min]])
+        patches.append(Polygon(coord, True, linewidth=1, edgecolor='black'))
+        patches.append(Polygon(coord_sym, True, linewidth=1, edgecolor='black'))
     x_min = number_of_full_cells*cell_length
     x_max = data_quasi[plot_index][-2]
     y_min = data_quasi[plot_index+number_of_full_cells+1][-1]
@@ -57,10 +62,15 @@ for plot_index in quasi_stages:
                       [x_min, y_max],
                       [x_max, y_max],
                       [x_max, y_min]])
-    patches.append(Polygon(coord, True))
+    coord_sym = np.array([[-x_min, y_min],
+                          [-x_min, y_max],
+                          [-x_max, y_max],
+                          [-x_max, y_min]])
+    patches.append(Polygon(coord, True, linewidth=2, edgecolor='black'))
+    patches.append(Polygon(coord_sym, True, linewidth=2, edgecolor='black'))
     p = PatchCollection(patches)
     ax.add_collection(p)
-    ax.set_xlim((0, length_max))
+    ax.set_xlim((-length_max, length_max))
     ax.set_ylim((-height_max, height_max))
     fig.savefig('output/time'+str(int(data_quasi[plot_index][1]))+'.png')
     plt.close()
